ff37d9d44b9b660abd8704b2773291f5f0f6322d
huahua <moflowerlkh@gmail.com>
2023-03-10 19:45:35 +0800

    test

---
+++ git_sync.sh
@@ -0,0 +1,9 @@
+git submodule foreach --recursive git add .
+git submodule foreach --recursive git commit -m $1
+git submodule foreach --recursive git push
+
+git add .
+git commit -m $1
+git push
+
+
