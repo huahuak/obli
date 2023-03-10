fc0949a70be9303091191f52c7c7de258bba0771
huahua <moflowerlkh@gmail.com>
2023-03-10 19:50:39 +0800

    git sync shell

--- git_sync.sh
+++ git_sync.sh
@@ -1,9 +1,7 @@
-git submodule foreach --recursive git add .
-git submodule foreach --recursive git commit -m $1
-git submodule foreach --recursive git push
+git submodule foreach --recursive git add . && \
+git submodule foreach --recursive git commit -m $1 && \
+git submodule foreach --recursive git push  && \
 
-git add .
-git commit -m $1
-git push
-
-
+git add . && \
+git commit -m $1 && \
+git push 
