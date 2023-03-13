f97b27bcf780be0963303e15e3ea9568fbb744ae
huahua <moflowerlkh@gmail.com>
2023-03-13 16:10:25 +0800

    sort in qemu ok!

--- git_sync.sh
+++ git_sync.sh
@@ -1,12 +1,7 @@
 git submodule foreach --recursive git add . && \
-echo "// ------------------------------------ //"
-git submodule foreach --recursive git commit -m "$1" & \
-echo "// ------------------------------------ //"
+git submodule foreach --recursive git commit -m "$1" && \
 git submodule foreach --recursive git push  && \
 
-echo "// ------------------------------------ //"
 git add . && \
-echo "// ------------------------------------ //"
 git commit -m "$1" && \
-echo "// ------------------------------------ //"
 git push 
